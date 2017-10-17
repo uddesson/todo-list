48eb4630601b97ead0be2ad6e59b391251d2e61f
Emma Uddesson <emma.uddesson@gmail.com>
2017-10-17 20:18:05 +0200

    Initial commit

---
+++ head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>To do-list</title>
+    <link rel=stylesheet type="text/css" href="style.css">
+</head>
+<header>
+    <h1>THE TO DO-LIST</h1>
+</header>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'head.php';?>
+<body>
+    <main> 
+        <footer>
+        </footer>
+    </main>
+    
+</body>
+</html>
