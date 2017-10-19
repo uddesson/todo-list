99291cb6010f7893f29b9f59a98f7833e04ff68e
Emma Uddesson <emma.uddesson@gmail.com>
2017-10-19 17:46:46 +0200

    Added some basic structure for the index-page

--- index.php
+++ index.php
@@ -3,40 +3,47 @@
 <?php include 'fetch_tasks.php'; ?>
 <body>
     <main> 
-         
-        <form action="index.php" method="POST">
-           <h2>Set a task:</h2>
-            <input type="text" name="title">
-            <br></br>
-            <input type="checkbox" name="completed" value="1">Done!<br>
-            <br></br>
-            <input type="submit">
-        </form>
-           
-           
-<!-- SKA COMMITA FETCH TASKS, TEST OCH TILLÄGG I LOOP -->
-           
+              
+        <div class="form_container">
+            <form action="index.php" method="POST">
+               <h2>Enter new task:</h2>
+                <input type="text" name="title" placeholder="Ex. Return books to library">
+                <br></br>
+                <!--Sucess-message to be shown when task is entered-->
+                <?= '<p>' . "Nice, you added a new task!" . '</p>' ?> 
+                <input type="submit" value="Add task">
+            </form>
+        </div>       
         
-        <div class="task_list_box">
-           <h1>TASKLIST</h1>
-            <?php
-                foreach($tasks as $tasks)
-                {
-                    echo '<p>' . '</br>';
-                    echo 'Task: ' . $tasks["title"] . '</p>';
-                }
-            ?>
-        </div>  
+        <div class="all_tasks_container">
+            <div class="current_tasks_container">
+               <h2>Current tasks:</h2>
+                
+                   <?php foreach($tasks as $tasks): ?>
+                    
+                       <div class="single_task_box">
+                           
+                            "Task": <?=$tasks["title"]?></div>
+                    
+                   <?php endforeach ?>
+                
+            </div> <!--Closing current task-container-->
+        
+            <div class="completed_tasks_container">
+                 <h2>Completed tasks:</h2>
+            </div>  
+        </div> <!--Closing all tasks-container-->
+          
+           
            
            
            <footer>
             <ul>
-                <li><a href="https://github.com/uddesson">github.com/uddesson/todo-list</a></li>
+                <li><a href="https://github.com/uddesson/todo-list">github.com/uddesson/todo-list</a></li>
                 <li><a href="https://github.com/uddesson">@uddesson</a></li>
                 <li><i class="fa fa-github" aria-hidden="true"></i></li>
             </ul>  
         </footer>
-    
     </main>
 <script src="https://use.fontawesome.com/67b80e2b65.js"></script>    
 </body>
