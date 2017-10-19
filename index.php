bab037e53d1baa6ff87de5c6897a804b5c1a1c9d
Emma Uddesson <emma.uddesson@gmail.com>
2017-10-19 13:33:29 +0200

    Added testform- and loop to see if fetching values in database works

--- index.php
+++ index.php
@@ -1,7 +1,35 @@
 <?php include 'head.php';?>
+<?php include 'database.php';?>
+<?php include 'fetch_tasks.php'; ?>
 <body>
     <main> 
-        <footer>
+         
+        <form action="index.php" method="POST">
+           <h2>Set a task:</h2>
+            <input type="text" name="title">
+            <br></br>
+            <input type="checkbox" name="completed" value="1">Done!<br>
+            <br></br>
+            <input type="submit">
+        </form>
+           
+           
+<!-- SKA COMMITA FETCH TASKS, TEST OCH TILLÄGG I LOOP -->
+           
+        
+        <div class="task_list_box">
+           <h1>TASKLIST</h1>
+            <?php
+                foreach($tasks as $tasks)
+                {
+                    echo '<p>' . '</br>';
+                    echo 'Task: ' . $tasks["title"] . '</p>';
+                }
+            ?>
+        </div>  
+           
+           
+           <footer>
             <ul>
                 <li><a href="https://github.com/uddesson">github.com/uddesson/todo-list</a></li>
                 <li><a href="https://github.com/uddesson">@uddesson</a></li>
