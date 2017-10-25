2e1605558c8294169028444b264e15d43d431886
Emma Uddesson <emma.uddesson@gmail.com>
2017-10-25 10:57:59 +0200

    Moved blocks of php from index, turning them into functions

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+require 'database.php';
+
+
+function check_if_task_is_entered()
+{
+    if(isset($_POST["submit_task"]))
+    {  
+        if (empty($_POST["task_title"]) || empty($_POST["added_by"]))
+        {
+            echo '<p>' . "You didn't add the task correctly!" . '</p>';
+            exit();
+        } 
+        
+        elseif (isset($_POST["task_title"]) && isset($_POST["added_by"]))
+        {
+            echo '<p>' . "Nice, you added a new task!" . '</p>';
+        }
+    }
+}
+
+
+function submit_task($pdo)
+{   
+        
+    if(isset($_POST["submit_task"]))
+    {    
+        $save_task = $pdo->prepare
+            (
+                "INSERT INTO todos (title, createdBy) 
+                VALUES (:title, :createdBy)"
+            );
+
+        $save_task->execute(array(
+            ":title"     => $_POST["task_title"],
+            ":createdBy"     => $_POST["added_by"], 
+        ));
+    }
+}
+
+
+
+
