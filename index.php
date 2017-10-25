77c8392008783bd0224f9c4229482eddfa45d90c
Emma Uddesson <emma.uddesson@gmail.com>
2017-10-25 10:38:56 +0200

    Changed structure, added function + new form for deleting tasks

--- index.php
+++ index.php
@@ -1,62 +1,97 @@
-<?php include 'head.php';?>
-<?php include 'database.php';?>
-<?php include 'insert_tasks.php'; ?>
-<?php include 'fetch_tasks.php'; ?>
+<?php require_once 'head.php';?>
+<?php require_once 'database.php';?>
+<?php require_once 'functions.php';?>
 
 <body>
-    <main> 
-  
-        <div class="form_container">
+<main> 
+    <div class="input_task_container">
+            
             <form action="index.php" method="POST">
-               <h2>Enter new task:</h2>
-                <input type="text" name="title" placeholder="Ex. Return books to library">
-                <br></br>
-                <input type="text" name="name" placeholder="Name">
-                <br></br>
-                <!--Sucess-message to be shown when task is entered-->
-                <?php
-                    if (isset($_POST["title"]) && isset($_POST["name"]) && !empty($_POST["title"]) && !empty($_POST["name"]))
-                    {
-                        echo '<p>' . "Nice, you added a new task!" . '</p>';
-                    }
-                ?> 
-                <input type="submit" value="Add task">
-            </form>
-        </div>       
+               
+                <h2>Enter new task:</h2>  
+                
+                <input type="text" name="task_title" placeholder="Ex. Return books to library"><br></br>
+                
+                <input type="text" name="added_by" placeholder="Added by"><br></br>
+                
+                <input id="submit_task" type="submit" name="submit_task" value="Add task">
+                
+                <?php check_if_task_is_entered(); ?> 
+                
+                <?php submit_task($pdo); ?>
+                
+            </form>   
+            
+        </div> <!--Closing input task-container -->
+        
         
         <div class="all_tasks_container">
+           
             <div class="current_tasks_container">
+              
                <h2>Current tasks:</h2>
                 
-                   <?php 
-                    foreach($tasks as $task): ?>
+               <?php 
+                require_once 'fetch_tasks.php';
                     
-                       <div class="single_task_box">
+                        foreach($tasks as $task): ?>                    
+                            
+                            <div class="single_task_box">
+                                
+                                <p>Task: <?=$task["title"]?>
+                                
+                                By: <?=$task["createdBy"]?></p>
+                                
+                                <form action="delete.php" method="POST">
+                                   
+                                    <input type="hidden" name="delete_id" value="<?= $task["id"]; ?>">
+                                    
+                                    <input id="delete_task" type="submit" value="Delete">
+                                    
+                                </form>    
+                                
+
+                                <input id="complete_task" type="submit" name="complete_task" value="Complete">
+                                
+                                
+                                
+                            </div>
                            
-                            <p>Task: <?=$task["title"]?>
-                            By: <?=$task["createdBy"]?>
-                            </p></div>
-                    
-                   <?php endforeach ?>
+                                         
+               <?php endforeach; ?>
+            
                 
             </div> <!--Closing current task-container-->
         
             <div class="completed_tasks_container">
-                 <h2>Completed tasks:</h2>
+                 
+                <h2>Completed tasks:</h2>
+                 
+                 
+                 
             </div>  
+            
         </div> <!--Closing all tasks-container-->
-          
+        
+
+        
            
+        <footer>
            
-           
-           <footer>
             <ul>
+               
                 <li><a href="https://github.com/uddesson/todo-list">github.com/uddesson/todo-list</a></li>
+                
                 <li><a href="https://github.com/uddesson">@uddesson</a></li>
+                
                 <li><i class="fa fa-github" aria-hidden="true"></i></li>
-            </ul>  
+                
+            </ul>
+              
         </footer>
+        
     </main>
+    
 <script src="https://use.fontawesome.com/67b80e2b65.js"></script>    
 </body>
 </html>
