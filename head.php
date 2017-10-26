63bb375179d231413b1fe491f3230edf04216c62
Emma Uddesson <emma.uddesson@gmail.com>
2017-10-26 21:57:57 +0200

    Smaller styling-changes

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
     <title>To do-list</title>
     <link rel=stylesheet type="text/css" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Comfortaa:700|Roboto:400,400i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:700%7CRoboto:400,400i" rel="stylesheet">
 </head>
 <header>
     <h1>THE TO DO-LIST <i class="fa fa-check" aria-hidden="true"></i></h1>
